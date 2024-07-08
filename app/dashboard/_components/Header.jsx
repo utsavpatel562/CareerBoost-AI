2d37de3e0b15903eb258edda4988c9bbe91ccdd6
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-08 10:18:43 -0400

    New Commit

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,30 +1,48 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 
-const Header = () => {
+function Header() {
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  });
   return (
     <>
       <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
         <Image
           src="/logo4.png"
-          width={40}
-          height={40}
+          width={35}
+          height={35}
           alt="CareerBoost-AI Logo"
         />
         <ul className="flex gap-6">
-          <li className="hover: text-primary hover: font-bold transition-all">
+          <li
+            className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
+              path == "/dashboard" && "text-primary font-bold"
+            }`}
+          >
             Dashboard
           </li>
-          <li>Resume Builder</li>
-          <li>Questions</li>
-          <li>Upgrade</li>
-          <li>How it works</li>
+          <li className="hover:text-primary hover:font-bold transition-all cursor-pointer">
+            Resume Builder
+          </li>
+          <li className="hover:text-primary hover:font-bold transition-all cursor-pointer">
+            Questions
+          </li>
+          <li className="hover:text-primary hover:font-bold transition-all cursor-pointer">
+            Upgrade
+          </li>
+          <li className="hover:text-primary hover:font-bold transition-all cursor-pointer">
+            How it works
+          </li>
         </ul>
         <UserButton />
       </div>
     </>
   );
-};
+}
 
 export default Header;
