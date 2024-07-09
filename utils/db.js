09efbb4a2235e6c01747b5807201c2b57e9ef78c
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-09 17:02:01 -0400

    New Commit

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-const sql = neon(process.env.DRIZZLE_DATABASE_URL);
-const db = drizzle(sql);
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+export const db = drizzle(sql);
