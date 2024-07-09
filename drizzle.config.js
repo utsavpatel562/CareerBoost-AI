c31403e7dd0a5411b32ea5c088b06d16d1eaabea
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-09 17:12:39 -0400

    New Commit

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  dialect: "postgresql", // "mysql" | "sqlite" | "postgresql"
+  schema: "./src/schema.ts",
+  dbCredentials:{
+    url:'postgresql://careerboostai_owner:UCXdrBvzH56w@ep-lively-band-a5d85a6q.us-east-2.aws.neon.tech/careerboostai?sslmode=require'
+  }
+};
