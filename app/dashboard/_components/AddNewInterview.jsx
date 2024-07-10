e7cd37bd83e6034740e494444f20590e339cd2d5
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-10 09:45:55 -0400

    New Commit

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AddNewInterview = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all">
+          <h2 className="font-bold text-lg text-center">Add New +</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddNewInterview;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,15 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
-
+import AddNewInterView from "./_components/AddNewInterview";
 function Dashboard() {
   return (
-    <div>
-      <h2>Dashboard</h2>
+    <div className="p-10">
+      <h2 className="font-bold text-2xl">Dashboard</h2>
+      <h2 className="text-gray-600">
+        Create and Start your AI Mockup Interview
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5">
+        <AddNewInterView />
+      </div>
     </div>
   );
 }
