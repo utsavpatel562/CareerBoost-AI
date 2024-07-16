2d1284f645c9f1f4410be7b2a5829d7da5175fa3
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-16 18:41:18 -0400

    New Commit

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { MockInterview } from "../../../../../utils/schema";
+import { db } from "../../../../../utils/db";
+import { eq } from "drizzle-orm";
 
-function StartInterview() {
+function StartInterview({ params }) {
+  useEffect(() => {
+    GetInterviewDetails();
+  }, []);
+  // Used to get interview details by MockID/Interview ID
+  const GetInterviewDetails = async () => {
+    try {
+      const result = await db
+        .select()
+        .from(MockInterview)
+        .where(eq(MockInterview.mockId, params.interviewId));
+      setInterviewData(result[0]);
+    } catch (error) {
+      console.error("Error fetching interview details:", error);
+    }
+  };
   return (
     <>
       <div>Start Interview</div>
