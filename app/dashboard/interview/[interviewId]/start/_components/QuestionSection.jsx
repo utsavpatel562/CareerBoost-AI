a3c3df7f018bf7fe531797adfebdb053080ce84a
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-18 18:47:02 -0400

    New Commit

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function QuestionSection() {
-  return <div>QuestionSection</div>;
+function QuestionSection({ mockInterviewQuestion }) {
+  return (
+    <>
+      <div className="p-5 border rounded-lg">
+        <div className="grid">
+          {mockInterviewQuestion &&
+            mockInterviewQuestion.map((question, index) => (
+              <h2 className="p-2 bg-secondary rounded-full">
+                Question #{index + 1}
+              </h2>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default QuestionSection;
