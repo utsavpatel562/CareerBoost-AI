768985b1a4e40889519ef7454007028de829ed47
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-19 18:45:47 -0400

    New Commit

--- app/dashboard/interview/[interviewId]/start/_components/RecordQuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordQuestionSection.jsx
@@ -1,26 +1,32 @@
 import Image from "next/image";
 import React from "react";
 import Webcam from "react-webcam";
+import { Button } from "../../../../../../components/ui/button";
 
 function RecordQuestionSection() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center rounded-lg p-5 my-20 bg-black">
-        <Image
-          src="/webcam3.png"
-          alt="WebCAM"
-          width={180}
-          height={180}
-          className="absolute"
-        />
-        <Webcam
-          mirrored={true}
-          style={{
-            height: 300,
-            width: 300,
-            zIndex: 300,
-          }}
-        />
+      <div className="flex items-center justify-center flex-col">
+        <div className="flex flex-col justify-center items-center rounded-lg p-5 mt-20 bg-black">
+          <Image
+            src="/webcam3.png"
+            alt="WebCAM"
+            width={140}
+            height={140}
+            className="absolute"
+          />
+          <Webcam
+            mirrored={true}
+            style={{
+              height: 300,
+              width: 300,
+              zIndex: 300,
+            }}
+          />
+        </div>
+        <Button variant="outline" className="my-10">
+          Record Answer
+        </Button>
       </div>
     </>
   );
