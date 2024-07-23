bec10ecde242aba3b92117ddb35279e039e5d9e7
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-23 16:01:09 -0400

    New Commit

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from "react";
 import { db } from "../../../../../utils/db";
 import { UserAnswer } from "../../../../../utils/schema";
 import { eq } from "drizzle-orm";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../../../../../components/ui/collapsible";
+import { ChevronsUpDown } from "lucide-react";
 
 function Feedback({ params }) {
   const [feedbackList, setFeedbackList] = useState([]);
@@ -34,6 +40,19 @@ function Feedback({ params }) {
           Find below interview question with correct answer, Your answer and
           feedback for improvement.
         </h2>
+        {feedbackList &&
+          feedbackList.map((item, index) => (
+            <Collapsible key={index}>
+              <CollapsibleTrigger className="p-2 bg-secondary rounded-lg my-2 flex justify-between text-left gap-5 max-w-[900px]">
+                {item.question}
+                <ChevronsUpDown className="h-5 w-5" />
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                Yes. Free to use for personal and commercial projects. No
+                attribution required.
+              </CollapsibleContent>
+            </Collapsible>
+          ))}
       </div>
     </>
   );
