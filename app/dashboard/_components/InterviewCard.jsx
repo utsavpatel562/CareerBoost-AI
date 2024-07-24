afd5245bbc1a4db7b5fb43d295e36d68187c2eb2
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-24 19:08:24 -0400

    New Commit

--- app/dashboard/_components/InterviewCard.jsx
+++ app/dashboard/_components/InterviewCard.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { Button } from "../../../components/ui/button";
 
 function InterviewCard({ interview }) {
   return (
     <div className="border shadow-sm rounded-lg p-3">
-      <h2 className="font-bold text-purple-700">{interview?.jobPosition}</h2>
+      <h2 className="font-bold text-blue-700">{interview?.jobPosition}</h2>
       <h2 className="text-sm text-gray-500">
         {interview?.jobExperience} Years of Experience
       </h2>
       <h2 className="text-xs text-gray-400">
         Created At: {interview?.createdAt}
       </h2>
+      <div className="flex justify-between mt-2">
+        <Button size="sm" variant="outline">
+          Feedback
+        </Button>
+        <Button size="sm" className="bg-blue-700 hover:bg-blue-800">
+          Start
+        </Button>
+      </div>
     </div>
   );
 }
--- app/dashboard/_components/InterviewHistory.jsx
+++ app/dashboard/_components/InterviewHistory.jsx
@@ -30,7 +30,7 @@ function InterviewHistory() {
   return (
     <div>
       <h2 className="font-medium text-xl">Previous Mock Interview</h2>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
         {interviewList &&
           interviewList.map((interview, index) => (
             <InterviewCard interview={interview} key={index} />
