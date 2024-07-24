d6a86739c4e095fa9afd76ffab8a8f0196c6934a
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-24 18:26:36 -0400

    New Commit

---
+++ app/dashboard/_components/InterviewCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InterviewCard = () => {
+  return <div>InterviewCard</div>;
+};
+
+export default InterviewCard;
--- app/dashboard/_components/InterviewHistory.jsx
+++ app/dashboard/_components/InterviewHistory.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../../../utils/db";
 import { MockInterview } from "../../../utils/schema";
 import { desc, eq } from "drizzle-orm";
+import InterviewCard from "../_components/InterviewCard";
 
 function InterviewHistory() {
   const { user } = useUser();
+  const [interviewList, setInterviewList] = useState([]);
 
   useEffect(() => {
     user && GetInterviewList();
@@ -21,11 +23,20 @@ function InterviewHistory() {
         eq(MockInterview.createdAt, user?.primaryEmailAddress?.emailAddress)
       )
       .orderBy(desc(MockInterview.id));
+
+    console.log(result);
+    setInterviewList(result);
   };
 
   return (
     <div>
       <h2 className="font-medium text-xl">Previous Mock Interview</h2>
+      <div>
+        {interviewList &&
+          interviewList.map((interview, index) => (
+            <InterviewCard key={index} />
+          ))}
+      </div>
     </div>
   );
 }
