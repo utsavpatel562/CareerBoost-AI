7bb49be57ec3006441b8212beb7e58a00175ce30
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-26 11:44:40 -0400

    New Commit

--- app/dashboard/_components/HomeStats.jsx
+++ app/dashboard/_components/HomeStats.jsx
@@ -9,12 +9,12 @@ const HomeStats = () => {
         </h2>
       </div>
       <div className="flex justify-center gap-7 p-7">
-        <div className="card bg-base-100 w-96 shadow-xl rounded-lg">
+        <div className="card bg-base-100 w-96 shadow-xl rounded-lg cursor-pointer">
           <figure>
             <img
               className="rounded-tr-lg rounded-tl-lg"
               src="/aiimg.jpg"
-              alt="Shoes"
+              alt="AI Image"
             />
           </figure>
           <div className="card-body p-6">
@@ -30,12 +30,12 @@ const HomeStats = () => {
             </p>
           </div>
         </div>
-        <div className="card bg-base-100 w-96 shadow-xl rounded-lg">
+        <div className="card bg-base-100 w-96 shadow-xl rounded-lg cursor-pointer">
           <figure>
             <img
               className="rounded-tr-lg rounded-tl-lg"
               src="/feedback1.jpg"
-              alt="Shoes"
+              alt="feedback"
             />
           </figure>
           <div className="card-body p-6">
@@ -48,24 +48,21 @@ const HomeStats = () => {
             </p>
           </div>
         </div>
-        <div className="card bg-base-100 w-96 shadow-xl rounded-lg">
+        <div className="card bg-base-100 w-96 shadow-xl rounded-lg cursor-pointer">
           <figure>
             <img
               className="rounded-tr-lg rounded-tl-lg"
-              src="/aiimg.jpg"
-              alt="Shoes"
+              src="/careergrowth.jpg"
+              alt="careerInsights"
             />
           </figure>
           <div className="card-body p-6">
             <h2 className="card-title text-lg font-bold flex gap-2">
-              AI-Driven Mock Interviews
-              <div className="badge badge-secondary font-light text-sm bg-cyan-600 text-gray-100 p-1 pr-3 pl-3 rounded-full">
-                NEW
-              </div>
+              Comprehensive Career Insights
             </h2>
             <p className="my-2">
-              Simulate real interview scenarios with AI to help you practice and
-              prepare effectively.
+              Access valuable insights and analytics to understand your areas
+              for improvement.
             </p>
           </div>
         </div>
