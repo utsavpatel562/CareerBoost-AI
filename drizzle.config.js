d2e4dc036daba8ccd8bd8b7bf720f4d53ee3727f
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-13 13:42:46 -0400

    first commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   dialect: "postgresql", // "mysql" | "sqlite" | "postgresql"
   schema: "./utils/schema.js",
   dbCredentials:{
-    url:'postgresql://careerboostai_owner:UCXdrBvzH56w@ep-lively-band-a5d85a6q.us-east-2.aws.neon.tech/careerboostai?sslmode=require'
+    url:'YOUR API KEY'
   }
 };
